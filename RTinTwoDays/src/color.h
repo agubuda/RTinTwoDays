84152f4058bc7e853b314e7004e4c691f6f75e94
agubuda <liu.wangyang@outlook.com>
2023-02-02 16:33:30 +0800

    finished reflection.

--- RTinTwoDays/src/color.h
+++ RTinTwoDays/src/color.h
@@ -13,9 +13,9 @@ void write_color(std::ostream& out, color pixel_color, int samples_per_pixel) {
 	//divide the color by the number of samples.
 
 	auto scale = 1.0 / samples_per_pixel;
-	r *= scale;
-	g *= scale;
-	b *= scale;
+	r = sqrt(scale * r);
+	g = sqrt(scale * g);
+	b = sqrt(scale * b);
 
 	//write the translated 0,255 value of each color component.
 
--- RTinTwoDays/src/sphere.h
+++ RTinTwoDays/src/sphere.h
@@ -7,8 +7,8 @@
 class sphere : public hittable {
 public:
 	sphere() {}
-	sphere(point3 cen, double r) 
-: center(cen), radius(r) {};
+	sphere(point3 cen, double r, shared_ptr<material> m) 
+		: center(cen), radius(r), mat_ptr(m) {};
 
 	virtual bool hit(
 		const ray& r, double t_min, double t_max, hit_record& rec
@@ -17,6 +17,7 @@ public:
 public:
 	point3 center;
 	double radius;
+	shared_ptr<material> mat_ptr;
 };
 
 bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
@@ -44,6 +45,7 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
 	//rec.normal = (rec.p - center) / radius;
 	vec3 outward_normal = (rec.p - center) / radius;
 	rec.set_face_normal(r, outward_normal);
+	rec.mat_ptr = mat_ptr;
 
 	return true;
 }
